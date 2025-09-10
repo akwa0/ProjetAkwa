b18f18435d8f61385b6c90a1a70280872419b7ae
akwa <l.poinambalom@interface3.be>
2025-09-10 15:44:57 +0200

    add entity Avis

---
+++ src/Entity/Avis.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nombreEtoile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $nombreCommentaire = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $dateCreation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombreEtoile(): ?int
+    {
+        return $this->nombreEtoile;
+    }
+
+    public function setNombreEtoile(?int $nombreEtoile): static
+    {
+        $this->nombreEtoile = $nombreEtoile;
+
+        return $this;
+    }
+
+    public function getNombreCommentaire(): ?int
+    {
+        return $this->nombreCommentaire;
+    }
+
+    public function setNombreCommentaire(?int $nombreCommentaire): static
+    {
+        $this->nombreCommentaire = $nombreCommentaire;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTime
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(\DateTime $dateCreation): static
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+}
