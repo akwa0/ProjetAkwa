17aae9600efdbfe7733332b7f1fa3d09799240b0
akwa <l.poinambalom@interface3.be>
2025-09-15 10:21:30 +0200

    fixtures add Avis

---
+++ src/DataFixtures/AvisFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Avis;
+use Faker;
+
+class AvisFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {$faker =Faker\Factory::create('fr-FR');
+        for ($i=0; $i<5 ; $i++){
+            $avis = new Avis();
+            $avis -> setNombreEtoile($faker->randomDigit());
+            $avis -> setNombreCommentaire($faker->randomDigit());
+            $avis -> setdateCreation($faker->dateTimeBetween('-10 years', 'now'));
+    
+            $manager->persist($avis);
+        }
+       
+
+        $manager->flush();
+    }
+}
