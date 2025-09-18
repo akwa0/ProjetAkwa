a84c439afb57f40d680c879be6cace1774403cd2
akwa <l.poinambalom@interface3.be>
2025-09-18 15:47:42 +0200

    FIX: accueil au lieu d'acceuil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+//importer les entities
+use App\Entity\Photo;
+
+final class AccueilController extends AbstractController
+{
+    // #[Route('accueil/index.html.twig', name: 'app_accueil')]
+    // public function testModele (EntityManagerInterface $em){
+    //     //on va obtenir ds entités de la BD 
+    //     //1.Obtenir le repode l'entité
+    //     $rep = $em->getRepository(Photo::class);
+    //     $photos = $rep->findAll();
+    //     // dd($photos);
+    //     $var =[
+    //         'photos'=>$photos,
+    //     ];
+    //     return $this->render('accueil/test_modele.html.twig', $var);
+    // }
+
+
+    #[Route('/accueil', name: 'app_accueil')]
+    public function testModele (EntityManagerInterface $em){
+        //on va obtenir ds entités de la BD 
+        //1.Obtenir le repode l'entité
+        $rep = $em->getRepository(Photo::class);
+        $photos = $rep->findAll();
+        // dd($photos);
+        $var =[
+            'photos'=>$photos,
+        ];
+        return $this->render('accueil/test_modele.html.twig', $var);
+    }
+
+
+}
