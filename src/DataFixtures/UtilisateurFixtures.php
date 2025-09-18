e866c21ff0c51dfab035ff6e6f47196f7e9c7485
akwa <l.poinambalom@interface3.be>
2025-09-18 14:13:13 +0200

    commit avant liaison avec utilisateur

--- src/DataFixtures/UtilisateurFixtures.php
+++ src/DataFixtures/UtilisateurFixtures.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Utilisateur;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class UtilisateurFixtures extends Fixture
 {
@@ -19,16 +20,29 @@ class UtilisateurFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {  
+        $faker = Factory::create('fr_FR');
+        
         for ($i = 0; $i<=5; $i++){
             $utilisateur = new Utilisateur();
-            //$utilisateur->setNom('nom'.$i);
-            //$utilisateur->setPrenom('prenom'.$i);
+            $utilisateur->setNom($faker->lastName);
+            $utilisateur->setDateNaissance($faker->dateTimeBetween('-70 years', '-18 years'));
             $utilisateur->setEmail('user'.$i.'@gmail.com');
             $utilisateur->setPassword($this->hasher->hashPassword($utilisateur,'password'.$i));
-            //$utilisateur ->setRoles();
+            $utilisateur ->setRoles(['ROLE_USER']);
             $manager->persist($utilisateur);
 
             $this->addReference("utilisateur".$i,$utilisateur); 
+        }
+        for ($i = 0; $i<=5; $i++){
+            $utilisateur = new Utilisateur();
+            $utilisateur->setNom($faker->lastName);
+            $utilisateur->setDateNaissance($faker->dateTimeBetween('-70 years', '-18 years'));
+            $utilisateur->setEmail('admin'.$i.'@gmail.com');
+            $utilisateur->setPassword($this->hasher->hashPassword($utilisateur,'password'.$i));
+            $utilisateur ->setRoles(['ROLE_ADMIN']);
+            $manager->persist($utilisateur);
+
+            $this->addReference("admin".$i,$utilisateur); 
         }
         
 
