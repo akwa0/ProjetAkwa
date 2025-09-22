434bbd27305c462d05bc2baa5d8498893b204c7a
akwa <l.poinambalom@interface3.be>
2025-09-22 09:49:11 +0200

    liens done

--- migrations/Version20250922074642.php
+++ migrations/Version20250922074642.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250918140247 extends AbstractMigration
+final class Version20250922074642 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,21 @@ final class Version20250918140247 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE avis (id INT AUTO_INCREMENT NOT NULL, nombre_etoile INT DEFAULT NULL, nombre_commentaire INT DEFAULT NULL, date_creation DATE NOT NULL, commentaire VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, contenu VARCHAR(255) DEFAULT NULL, date_envoie DATE DEFAULT NULL, etat TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE avis (id INT AUTO_INCREMENT NOT NULL, utilisateurs_id INT DEFAULT NULL, nombre_etoile INT DEFAULT NULL, nombre_commentaire INT DEFAULT NULL, date_creation DATE NOT NULL, commentaire VARCHAR(255) DEFAULT NULL, INDEX IDX_8F91ABF01E969C5 (utilisateurs_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE message (id INT AUTO_INCREMENT NOT NULL, utilisateurs_id INT DEFAULT NULL, contenu VARCHAR(255) DEFAULT NULL, date_envoie DATE DEFAULT NULL, etat TINYINT(1) NOT NULL, INDEX IDX_B6BD307F1E969C5 (utilisateurs_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, photo_poster_id INT DEFAULT NULL, date_creation DATE NOT NULL, description LONGTEXT DEFAULT NULL, auteur VARCHAR(255) NOT NULL, INDEX IDX_14B7841856483A24 (photo_poster_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE utilisateur (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, nom VARCHAR(255) DEFAULT NULL, date_naissance DATE DEFAULT NULL, experience INT DEFAULT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE avis ADD CONSTRAINT FK_8F91ABF01E969C5 FOREIGN KEY (utilisateurs_id) REFERENCES utilisateur (id)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F1E969C5 FOREIGN KEY (utilisateurs_id) REFERENCES utilisateur (id)');
         $this->addSql('ALTER TABLE photo ADD CONSTRAINT FK_14B7841856483A24 FOREIGN KEY (photo_poster_id) REFERENCES utilisateur (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE avis DROP FOREIGN KEY FK_8F91ABF01E969C5');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307F1E969C5');
         $this->addSql('ALTER TABLE photo DROP FOREIGN KEY FK_14B7841856483A24');
         $this->addSql('DROP TABLE avis');
         $this->addSql('DROP TABLE message');
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,7 +11,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Extension\Core\Type\TextType;use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class RegistrationFormType extends AbstractType
