10fefea5f9c1f880d95489df7e3a4c91d450cabe
Akwa <poinambalom.l@gmail.com>
2025-09-25 15:18:01 +0200

    test css js sur accueil + reglement probleme

---
+++ public/accueil/script.js
@@ -0,0 +1,6 @@
+    window.addEventListener("load", () => {
+        setTimeout(() => {
+        document.getElementById("btnContainer").classList.add("show");
+        }, 2000); // correspond à la durée de l'animation du logo
+    });
+    
